52c765a04867d885619ed289655b406125966670
HARSHIK007 <tantiherik@gmail.com>
2025-03-27 22:00:35 +0530

    search page addon

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const reviewRoute = require("./routes/review.js");
 const userRoute = require("./routes/user.js");
 
   // const mongoUrl = "mongodb://127.0.0.1:27017/wanderlust";
-  const dbUrl = process.env.ATLASDB_URL;
+  const dbUrl = 'mongodb://127.0.0.1:27017/wanderlust';
   ;
 
 main()
@@ -46,20 +46,20 @@ app.use(express.json());
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 
-const store = MongoStore.create({
-  mongoUrl: dbUrl,
-  crypto: {
-    secret: process.env.SECRET,
-  },
-  touchAfter: 24 * 3600
-})
+// const store = MongoStore.create({
+//   mongoUrl: dbUrl,
+//   crypto: {
+//     secret: process.env.SECRET,
+//   },
+//   touchAfter: 24 * 3600
+// })
 
-store.on("error", () => {
-  console.log("ERROR IN MONGO SESSION STORE", err)
-})
+// store.on("error", () => {
+//   console.log("ERROR IN MONGO SESSION STORE", err)
+// })
 
 const sessionOptions = {
-  store,
+  // store,
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
