68e0f78315e6aa61311bdb23d3734722c9ddb2be
HARSHIK007 <tantiherik@gmail.com>
2025-03-28 22:39:54 +0530

    server error fixed link

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const listingRoute = require("./routes/listing.js");
 const reviewRoute = require("./routes/review.js");
 const userRoute = require("./routes/user.js");
 
-const dbUrl = "mongodb://127.0.0.1:27017/wanderlust"//process.env.ATLASDB_URL;
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then((res) => {
@@ -44,20 +44,20 @@ app.use(express.json());
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 
-// const store = MongoStore.create({
-//   mongoUrl: dbUrl,
-//   crypto: {
-//     secret: process.env.SECRET,
-//   },
-//   touchAfter: 24 * 3600
-// })
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600
+})
 
-// store.on("error", () => {
-//   console.log("ERROR IN MONGO SESSION STORE", err)
-// })
+store.on("error", () => {
+  console.log("ERROR IN MONGO SESSION STORE", err)
+})
 
 const sessionOptions = {
-  // store,
+  store,
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
