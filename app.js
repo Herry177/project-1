40a07a3e6db98835f7dca17676fc5515ab18fac3
Harshik177 <harshikt00004@gmail.com>
2025-09-24 02:59:02 +0530

    login and sign up issue fixed

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ const sessionOptions = {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.ATLASDB_URL
+    callbackURL:"https://project-1-vp72.onrender.com/auth/google/callback"
   },
   
   async function(accessToken, refreshToken, profile, done) {
